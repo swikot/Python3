dc543e1bf37e61a79a02d470f2eafd5f65584f58
swikot <swikot@gmail.com>
2016-10-10 00:39:53 +0600

    fifth commit

--- SetsAndFrozenSets.py
+++ SetsAndFrozenSets.py
@@ -1,4 +1,66 @@
-__author__ = 'snow'
+__author__ = ''
 
 # hi sets starting point
+
+#  set contains an unordered collection of unique and immutable objects
+
+# Sets
+
+
+# If we want to create a set, we can call the built-in set function with a sequence or another iterable object:
+
+
+x=set("my name is swikot")
+print(x)
+# {'w', 'i', 'o', 'y', 's', 'a', ' ', 'm', 'n', 'k', 't', 'e'}
+print(type(x))
+
+
+# Building set with lists
+
+
+
+
+y=set(["java","pearl","python"])
+print(y)
+# {'java', 'pearl', 'python'}
+# no list brackets
+
+
+# immutable sets
+# Sets are implemented in a way, which doesn't allow mutable objects
+# we cannot include for example lists as elements:
+
+
+
+cities = set((("Python","Perl"), ("Paris", "Berlin", "London")))
+
+print(cities)
+# cities = set((["Python","Perl"], ["Paris", "Berlin", "London"]))  prohabited
+
+
+
+# Though sets can't contain mutable objects, sets are mutable:
+city = set(["Frankfurt", "Basel","Freiburg"])
+city.add("dhaka")
+print(city)
+
+
+# frozen
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
