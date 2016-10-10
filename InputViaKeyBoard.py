156e5ed9a3d4c7a5adb153a7b2abd76d5ca48dab
swikot <swikot@gmail.com>
2016-10-10 14:39:33 +0600

    sixth commit

--- InputViaKeyBoard.py
+++ InputViaKeyBoard.py
@@ -1,3 +1,34 @@
 __author__ = 'snow'
 
 
+# The input of the user will be returned as a string without any changes
+# If this raw input has to be transformed into
+# another data type needed by
+# the algorithm, we can use either
+# a casting function or the eval function.
+
+# name=input("give me some input")
+# print("your output= :",name," ",type(name))
+# name1=eval(input("Input me something !!!"))
+# print("your output= :",name1,type(name1))
+# population=input("input me some thing !!! :")
+# print("population",population," ",type(population))
+population=eval(input("give me some population!!"))
+print("population",population," ",type(population))
+
+
+
+#  in python 3 i have to use only input() or eval(input())
+
+
+
+
+
+
+
+
+
+
+
+
+
