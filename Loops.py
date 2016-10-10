09128df3bf5cc1646ba2d5be2ec99c513938df91
swikot <swikot@gmail.com>
2016-10-10 17:02:15 +0600

    sixth commit

---
+++ Loops.py
@@ -0,0 +1,28 @@
+__author__ = 'snow'
+
+
+
+#1 Count-controlled loops
+# for (i=0; i <= n; i++)
+# Python doesn't know this kind of loop.
+
+#2 Condition-controlled loop
+# A loop will be repeated until a given condition changes
+# There are while loops and do while loops with this behaviour.
+
+# 3Collection-controlled loop
+# This is a special construct which allow looping through the elements of a "collection"
+#  which can be an array, list or other ordered sequence.
+
+# Python supplies two different kinds of loops:
+# the while loop and the for loop,
+# which correspond to the condition-controlled loop and collection-controlled loop.
+
+
+
+
+
+
+
+
+
