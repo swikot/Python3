d41baf9a3cf852c7db0aff1eddbfbc2dc2151988
swikot <swikot@gmail.com>
2016-10-10 16:49:30 +0600

    sixth commit

--- MaxFunction.py
+++ MaxFunction.py
@@ -1,8 +1,29 @@
 __author__ = 'snow'
 
-x = eval(input("1st Number: "))
-y = eval(input("2nd Number: "))
-z = eval(input("3rd Number: "))
+# x = eval(input("1st Number: "))
+# y = eval(input("2nd Number: "))
+# z = eval(input("3rd Number: "))
+#
+#
+# maximum=max((x,y,z))
+# mximum2=maximum
+# print("maximum=" +str(maximum))
+# print("maxumum2=",mximum2)
+#
 
-maximum=max((x,y,z))
-print("maximum=" +str(maximum))
+print()
+
+number_of_values = int(input("How many values? "))
+
+maximum = float(input("Value: "))
+print(maximum)
+for i in range(number_of_values - 1):
+    value = float(input("Value: "))
+    if value > maximum:
+        maximum = value
+
+print("The maximal value is: " + str(maximum))
+
+
+
+
