d76ff9f958ea49b0ecf78c9e3bc01bd0d58a6208
swikot <swikot@gmail.com>
2016-10-10 15:36:11 +0600

    sixth commit

--- ConditionalStatements.py
+++ ConditionalStatements.py
@@ -1,1 +1,50 @@
 __author__ = 'snow'
+
+
+# if contditon:
+#     something
+#     something
+#     something
+
+
+nationality=input("Nationality?: ")
+
+# if nationality=="french" or nationality=="French":
+#     print(True)
+#
+#
+# if nationality=="italian" or nationality=="Italian":
+#     print(True)
+#
+# else:
+#     print(False)
+
+
+
+if nationality=="french" or nationality=="French":
+    print(True)
+
+
+elif nationality=="italian" or nationality=="Italian":
+    print(True)
+
+else:
+    print("you are neither French or Italian")
+
+
+# General form of pythom if elif else
+
+# if condition_1:
+#     statement_block_1
+# elif condition_2:
+#     statement_block_2
+#
+# ...
+#
+# elif another_condition:
+#     another_statement_block
+# else:
+#     else_block
+
+
+
