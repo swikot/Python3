cca478ae43eac52df40b1043446efa81545772a5
swikot <swikot@gmail.com>
2016-10-14 11:42:06 +0600

    7th commit

--- ForLoops.py
+++ ForLoops.py
@@ -1,1 +1,29 @@
 __author__ = 'snow'
+
+
+
+edibles = ["ham","spam","eggs","nuts"]
+
+for i in edibles:
+    if i=="spam":
+        print("no more spam")
+        break
+    print("delicious food ",i)
+
+else:
+    print("print me ")
+
+print("wait for your call")
+print()
+
+
+
+
+
+
+
+
+
+
+
+
