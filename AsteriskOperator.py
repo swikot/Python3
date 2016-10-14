567120ae111ac7dd40d1776eca8ed1e4cbefc0a7
swikot <swikot@gmail.com>
2016-10-15 01:28:08 +0600

    7th commit

--- AsteriskOperator.py
+++ AsteriskOperator.py
@@ -57,6 +57,31 @@ print("now my List can be written as= : ",my_list)
 
 
 
+def sum(a,b):  #receive args from function calls as sum(1,2) or sum(a=1,b=2)
+    print(a+b)
+
+my_tuple = (1,2)
+my_list = [1,2]
+my_dict = {'a':1,'b':2}
+
+# Let us unpack data structure of list or tuple or dict into arguments with help of '*' operator
+sum(*my_tuple)   # becomes same as sum(1,2) after unpacking my_tuple with '*'
+sum(*my_list)    # becomes same as sum(1,2) after unpacking my_list with  '*'
+sum(**my_dict)   # becomes same as sum(a=1,b=2) after unpacking by '**'
+
+# output is 3 in all three calls to sum function.
+# http://stackoverflow.com/questions/36901/what-does-double-star-and-star-do-for-python-parameters
+
+
+
+
+
+
+
+
+
+
+
 
 
 
