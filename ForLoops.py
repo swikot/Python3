b36556df94cae2042718570946b1ef5aa80df40c
swikot <swikot@gmail.com>
2016-10-14 12:08:40 +0600

    7th commit

--- ForLoops.py
+++ ForLoops.py
@@ -1,4 +1,7 @@
+import math
+
 __author__ = 'snow'
+
 
 
 
@@ -15,6 +18,48 @@ else:
 
 print("wait for your call")
 print()
+
+
+
+
+# pithagorus theorem
+fibonacci=[0,1,1,2,3,5,8,13]
+
+for i in range(len(fibonacci)):
+    print(fibonacci[i],end=" ")
+
+
+
+print("\n")
+for i in fibonacci:
+    print(i,end=" ")
+
+# List iteration with Side Effects
+
+colours = ["red"]
+for i in colours:
+    if i == "red":
+        colours += ["black"]
+    if i == "black":
+        colours += ["white"]
+print(colours)
+
+
+
+# using slicing operator It can be removed
+
+colourss=["red"]
+for i in colourss[:]:
+    if i=="red":
+        colourss+=["black"]
+    if i=="black":
+        colourss+="white"
+
+
+print(colourss)
+
+
+
 
 
 
