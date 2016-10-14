e3f42f51ba84a0cf59ddfdb0f7fbd42178c52967
swikot <swikot@gmail.com>
2016-10-14 14:14:46 +0600

    7th commit

---
+++ Functions.py
@@ -0,0 +1,41 @@
+__author__ = 'Swikot'
+
+# general syntax of function
+# def function-name(Parameter list):
+#     statements, i.e. the function body
+
+# simple function
+def function_name():
+    print("swikot")
+
+function_name()
+
+# calculating farhenhite
+
+
+def farenhite(T_degrre_celcious):
+    '''reture the temperature in degree farhenhite'''
+    return T_degrre_celcious
+
+print()
+for i in (22.3,23.4,25.6):
+    print(i," : ",farenhite(i))
+
+
+# Optional Parameter
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
