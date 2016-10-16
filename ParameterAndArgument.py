4e3429d5aaa1e7054ebb4451203a8835e1fc5042
swikot <swikot@gmail.com>
2016-10-16 22:25:44 +0600

    7th commit

---
+++ ParameterAndArgument.py
@@ -0,0 +1,19 @@
+__author__ = 'snow'
+
+def function(x):
+     print("x=: ",x,"  id=: ",id(x))
+     x=42
+     print("x=: ",x,"  id=: ",id(x))
+
+
+
+
+p=6
+print("value of p is = : ",p)
+function(p)
+
+
+print("now the value of p is: ",p)
+
+
+
---
+++ Recursion.py
@@ -0,0 +1,15 @@
+__author__ = 'snow'
+
+
+def factorial(n):
+    if n==1:
+        return 1
+    elif n<=0:
+        return 1
+    else:
+        return n*factorial(n-1)
+
+print(factorial(7))
+
+
+
