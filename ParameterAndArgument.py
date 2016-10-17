55ceaf99852ee5c08c670194d7a440556e5e6e9c
swikot <swikot@gmail.com>
2016-10-17 12:47:23 +0600

    7th commit

--- ParameterAndArgument.py
+++ ParameterAndArgument.py
@@ -16,4 +16,78 @@ function(p)
 print("now the value of p is: ",p)
 
 
+def varpara(*x):
+    print(x)
+
+
+varpara(34,34,45)
+
+
+
+
+
+# * function call
+
+def func(x,y,z):
+    print(x,y,z)
+
+
+p=(10,20,30)
+q=(11,2,3)
+func(*p)
+func(q[0],q[1],q[2])
+
+# Arbitrary Keyword Parameters
+
+def f(**args):
+    print(args)
+
+
+f(de="Germnan",en="English",fr="French")
+
+
+# Double Asterisk in Function Calls
+
+
+def variable(a,b,x,y):
+    print(a,b,x,y)
+
+
+
+d = {'a':'append', 'b':'block','x':'extract','y':'yes'}
+
+
+variable(**d)
+
+# combination can posiible
+
+# def ()
+#
+# t = (47,11)
+# d = {'x':'extract','y':'yes'}
+# fac(*t, **d)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
