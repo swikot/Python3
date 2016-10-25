f0a2a588757c09b220661f400cbf5ad2db60a9ce
swikot <swikot@gmail.com>
2016-10-25 15:38:45 +0600

    8th commit

--- OOP/Destructor.py
+++ OOP/Destructor.py
@@ -1,1 +1,23 @@
 __author__ = 'snow'
+# the method __del__
+
+class Robot():
+
+    def __init__(self, name):
+        print(name + " has been created!")
+
+    def __del__(self):
+        print ("Robot has been destroyed")
+
+
+if __name__ == "__main__":
+    x = Robot("Tik-Tok")
+    y = Robot("Jenkins")
+    z = x
+    print("Deleting x")
+    del x
+    print("Deleting z")
+    del z
+    del y
+
+
