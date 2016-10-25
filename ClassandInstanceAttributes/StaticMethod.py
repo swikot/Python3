d9455719ed841c4f0a5331a732cd0409460eb135
swikot <swikot@gmail.com>
2016-10-25 17:21:50 +0600

    8th commit

--- ClassandInstanceAttributes/StaticMethod.py
+++ ClassandInstanceAttributes/StaticMethod.py
@@ -1,3 +1,47 @@
-__author__ = 'snow'
+__author__ = 'swikot'
+
+# class Robot:
+#     __counter = 0
+#
+#     def __init__(self):
+#         type(self).__counter += 1
+#
+#     def RobotInstances(self):
+#         return Robot.__counter
+#
+#
+# if __name__ == "__main__":
+#     x = Robot()
+#     print(x.RobotInstances())
+#     y = Robot()
+#     print(x.RobotInstances())
+#     Robot.RobotInstances()
+
+
+
+class Robot:
+    __counter=0
+    def __init__(self):
+        type(self).__counter+=1
+
+    @staticmethod
+    def RobotInstance():
+        return Robot.__counter
+
+
+
+if __name__=="__main__":
+    print(Robot.RobotInstance())
+    x=Robot()
+    print(x.RobotInstance())
+    y=Robot()
+    print(y.RobotInstance())
+    print(Robot.RobotInstance())
+
+
+
+
+
+
 
 
